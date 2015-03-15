5c43f9186e327b83b71baf461425bcad57847576
Frank Calise <fcalise@gmail.com>
2015-03-14 20:20:47 -0400

    implemented explode tabs, run background

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Chrome extension to add keyboard shortcuts for managing multiple Chrome windows
 
 ## Shortcuts
 
-* Ctrl+Shift+# - Move to window # (1 through 8, 9 always goes to last window like tab functionality Ctrl+#)
 * Ctrl+Shift+Home - Join all tabs in current window to the main window
 * Ctrl+Shift+End - [not implemented yet] Move current tab and tabs to right of it to a new window
+* Ctrl+Shift+E - [not implemented yet] Explode current tab to a new window
--- js/background.js
+++ js/background.js
@@ -9,6 +9,10 @@ chrome.commands.onCommand.addListener(function(command) {
 
 	if (command == 'join_current_window') {
 		joinWindow();
+	} else if (command == 'explode_current_tab') {
+		explodeTab();
+	} else if (command == 'explode_tabs_to_right') {
+		explodeTabsToRight();
 	}
 });
 
@@ -27,6 +31,10 @@ function setFocusWindow(index) {
 	});
 }
 
+/**
+ * Take all tabs from the current Chrome window and move
+ * them to the end of the main (first open) Chrome window.
+ */
 function joinWindow() {
 	var win = chrome.windows.getCurrent({ populate: true }, function(win) {
 		var tabIds = [];
@@ -42,8 +50,44 @@ function joinWindow() {
 			chrome.tabs.move(tabIds, { windowId: wins[0].id, index: -1 });
 		});
 	});
+
+	// TODO - select first joined tab
 }
 
-function explodeWindow() {
-	// TODO - move tabs from current tab and to the right to a new chrome window
+/**
+ * Pop the current tab out to a new Chrome window.
+ */
+function explodeTab() {
+	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+		var win = chrome.windows.create({ tabId: tabs[0].id });
+	});
+}
+
+/**
+ * Take all tabs to the right of the active one (including the active)
+ * and move to a new Chrome window.
+ */
+function explodeTabsToRight() {
+	// get current tab index and number of tabs
+	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+		var currentTabIdx = tabs[0].index;
+
+
+		var win = chrome.windows.getCurrent({ populate: true }, function(win) {
+			var tabIds = [];
+			
+			// build tab id array
+			win.tabs.forEach(function(tab) {
+				if (tab.index >= currentTabIdx) {
+					tabIds.push(tab.id);
+				}
+			});		
+
+			// move the tabs (let first assume new tab position)
+			chrome.windows.create({ tabId: tabIds.shift() }, function(newWin) {
+				// move the remaining tabs to the end of the new window
+				chrome.tabs.move(tabIds, { windowId: newWin.id, index: -1 });
+			});
+		});
+	});
 }
